41382e1cf3e6f8918d7a7bab6025e14870c88c95
Renato CRON <renato.cron@gmail.com>
2011-12-03 18:10:35 -0200

    script .sh para atualizar os indicadores

---
+++ script/xls2csv/indicadores-2010-2011.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+perl convert-soma-mensal.pl ../../data/secretaria-seguranca-publica-rs/xls/1321896360Indicadores___SSP___Municipios_2010___08.11.11.xls
+perl convert-soma-mensal.pl ../../data/secretaria-seguranca-publica-rs/xls/1321897182Indicadores___SSP___Municipios___2011___Atualizacao_em_8.11.2011___Jan_a_Out.xls
+
