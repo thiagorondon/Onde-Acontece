98b7d4812586f0cef4311aaa37c36432cac78e04
Gabriel A. Santana <gabiruh@gmail.com>
2011-12-03 17:40:34 -0200

    - Dump da tabela com os todos os municipios do Brasil - Schema
    Signed-off-by: Gabriel A. Santana <gabiruh@gmail.com>

---
+++ lib/Onde/Acontece/Schema.pm
@@ -0,0 +1,20 @@
+use utf8;
+package Onde::Acontece::Schema;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+
+# Created by DBIx::Class::Schema::Loader v0.07014 @ 2011-12-03 17:35:07
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:W9NUBzvlEmUpkyJ0sHEUVA
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
