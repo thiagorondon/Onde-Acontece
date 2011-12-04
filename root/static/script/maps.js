f4873691582d9f1eaecb528746c26baa5d219f67
Gabriel A. Santana <gabiruh@gmail.com>
2011-12-04 06:40:21 -0200

    - atualizando dados no mapa
    Signed-off-by: Gabriel A. Santana <gabiruh@gmail.com>

--- root/static/script/maps.js
+++ root/static/script/maps.js
@@ -8,16 +8,43 @@
   // $('svg path').live('hover', function(e){
   //   $(e.target).tipsy('show');
   // });
-//  d3.json("/poligono" + window.location.pathname.replace(/\/.*?\//, '/'), function(json) {
-  d3.json("/api/seguranca/RS", function(json) {
-            console.log(json);
+  //  d3.json("/poligono" + window.location.pathname.replace(/\/.*?\//, '/'), function(json) {
+  d3.json("/api/seguranca/RS?o_id=1&ano=2011&content-type=application/json", function(json) {
     region.selectAll("path").data(json.features).enter().append("svg:path").attr("d", path).attr('class', quantize).attr('original-title', function(d) {
-      $(this).tipsy({gravity: $.fn.tipsy.autoNS});
-      return d.properties.name;
+      $(this).tipsy({
+        gravity: $.fn.tipsy.autoNS
+      });
+      return d.properties.name + ': ' + d.properties.quant;
     });
 
   });
+
+  function fetch(type_id, year) {
+    d3.json("/api/seguranca/RS?o_id=$$type&ano=$$year&content-type=application/json".replace('$$year', year).replace('$$type', type_id), function(json) {
+      $('#chart svg').remove();
+      var region = d3.select("#chart").append("svg:svg").attr("id", "region").attr("class", "Reds").attr('width', '450px').attr('height', '400px');
+
+      region.selectAll("path").data(json.features).enter().append("svg:path").attr("d", path).attr('class', quantize).attr('original-title', function(d) {
+      $(this).tipsy({
+        gravity: $.fn.tipsy.autoNS
+      });
+
+
+        return d.properties.name + ': ' + d.properties.quant;
+      });
+
+    });
+
+  }
+
+  $("select[name='ocorrencia']").change(function(e) {
+    fetch($("select[name='ocorrencia'] option:selected").val(), $("select[name='ano'] option:selected").val());
+  });
+  $("select[name='ano']").change(function(e) {
+    fetch($("select[name='ocorrencia'] option:selected").val(), $("select[name='ano'] option:selected").val());
+  });
+
   function quantize(d) {
-    return "q" + Math.min(8, ~~ ((d.properties.name.length * 9) / 12)) + "-9";
+    return "q" + Math.max(1, ((d.properties.quant % 42)));
   }
 })();
