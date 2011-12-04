effc4d4010d318e1a5ef22ef21f67ad13c60970b
Eden Cardim <edencardim@gmail.com>
2011-12-04 05:06:20 -0200

    servindo json das ocorrencias

--- lib/Onde/Acontece/Controller/API/Seguranca/Estado.pm
+++ lib/Onde/Acontece/Controller/API/Seguranca/Estado.pm
@@ -3,6 +3,7 @@ use Moose;
 use namespace::autoclean;
 
 BEGIN { extends 'Catalyst::Controller::REST'; }
+use JSON;
 
 sub base : Chained('/api/seguranca/base') PathPart('') CaptureArgs(0) {
   my ( $self, $c ) = @_;
@@ -18,17 +19,43 @@ sub view : Chained('object') : PathPart('') : Args(0) ActionClass('REST') {
   my ( $self, $c ) = @_;
 }
 
+use Data::Dump;
+
 sub view_GET {
   my ( $self, $c ) = @_;
-  my $state = $c->stash->{estado}->search(
-    { },
-    { prefetch => { municipios => 'ocorrencias_municipio' },
+  my $o_id  = $c->req->query_parameters->{o_id};
+  my $ano   = $c->req->query_parameters->{ano};
+  my $state = $c->stash->{estado}->related_resultset('municipios')->search(
+    { 'ocorrencia.id' => $o_id, 'ocorrencias_municipio.ano' => $ano },
+    { '+select'    => [ \'st_asgeojson(municipios.the_geom)' ],
+      '+as'        => ['geojson'],
+      prefetch     => [ { 'ocorrencias_municipio' => 'ocorrencia' } ],
       order_by     => [qw(ocorrencias_municipio.ano)],
       result_class => 'DBIx::Class::ResultClass::HashRefInflator'
     }
   );
 
-  $self->status_ok( $c => entity => [ $state->all ] );
+  #  $self->status_ok($c => entity => [$state->all]);
+
+  $self->status_ok(
+    $c,
+    entity => {
+      type     => 'FeatureCollection',
+      features => [
+        map {
+          +{type       => 'Feature',
+            id         => $_->{ocorrencias_municipio}[0]{municipio_id},
+            geometry   => decode_json($_->{geojson}),
+            properties => {
+              name  => $_->{nome},
+              quant => $_->{ocorrencias_municipio}[0]{quant}
+            }
+          }
+        } $state->all
+      ]
+    }
+  );
+
 }
 
 __PACKAGE__->meta->make_immutable;
