39e8f4f4f541c0c8ad7aec5b96c230698b2f96c3
Gabriel A. Santana <gabiruh@gmail.com>
2011-12-09 11:02:18 -0200

    - "Carregando mapa, aguarde..." - ajustando valores com x/sqrt(x) - Signed-off-by: Gabriel A. Santana <gabiruh@gmail.com>

--- root/static/script/maps.js
+++ root/static/script/maps.js
@@ -21,17 +21,30 @@
     console.log($(e.target));
   });
   function fetch(type_id, year) {
+    region.append("svg:text").attr("text-anchor", "middle").attr("x", 220)
+      .attr("y", 150).text('Carregando mapa, aguarde...');
+
     d3.json("/api/seguranca/RS?o_id=$$type&ano=$$year&content-type=application/json".replace('$$year', year).replace('$$type', type_id), function(json) {
       $('#chart svg').remove();
+
       var region = d3.select("#chart").append("svg:svg").attr("id", "region").attr("class", "Reds").attr('width', '450px').attr('height', '400px');
-       region.selectAll("path").data(json.features).enter().append("svg:path").attr("d", path).attr('fill', quantize).attr('original-title', function(d){
-        return d.properties.name + ': ' +  (d.properties.quant+'').slice(0, 5);
+      region.selectAll("path").data(json.features).enter().append("svg:path").attr("d", path).attr('fill', quantize).attr('original-title', function(d) {
+        return d.properties.name + ': ' + (d.properties.quant + '').slice(0, 5);
       }).classed('current', function(d) {
         return d.properties.name == $('select[name=municipio] option:selected').val();
+      }).on("mouseover", function(d, i) {
+        d3.select(this).transition().duration(300).style("fill", "#00f1f1");
+      }).on("mouseout", function(d, i) {
+        d3.select(this).transition().duration(300).style("fill", quantize);
+      });;
+      $('path').qtip({
+        content: {
+          attr: 'original-title'
+        },
+        position: {
+          target: 'mouse'
+        }
       });
-      $('path').qtip({content :{attr: 'original-title'}, position: {
-                        target: 'mouse'
-      }});
     });
 
   }
@@ -40,13 +53,13 @@
   var type_id = $('select[name=ocorrencia] option:selected').val();
 
   fetch(type_id, year);
-   var lowest = '#F8DBCD';
-   var largest = '#C43C35';
-  var changeColor = d3.interpolateRgb(lowest, largest);
+  var lowest = '#F8DBCD';
+  var highest = '#C43C35';
+  var changeColor = d3.interpolateRgb(lowest, highest);
   function quantize(d) {
     var x = d.properties.quant;
-    if (x==0)
-      return lowest;
-    return changeColor(x*10); // changeColor(x/Math.sqrt(x));
+    if (x == 0) return lowest;
+    if (x > 1.0) return highest;
+    return changeColor(x / Math.sqrt(x));
   }
 })();
