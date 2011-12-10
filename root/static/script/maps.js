bf0adea7055e1291d1a10a49be902f9cddf92ca8
Gabriel A. Santana <gabiruh@gmail.com>
2011-12-09 23:36:17 -0200

    - Cursor no hover do municipio - clique no municipio realiza um request - movendo um pouco a posição do tooltip pra que ele nao capture o   evento de clique - removendo mensagem de espera, a que thiago fez ficou melhor :)
    Signed-off-by: Gabriel A. Santana <gabiruh@gmail.com>

--- root/static/script/maps.js
+++ root/static/script/maps.js
@@ -5,44 +5,43 @@
   path.projection(proj);
   var region = d3.select("#chart").append("svg:svg").attr("id", "region").attr("class", "Reds").attr('width', '450px').attr('height', '400px');
 
-  // $('svg path').live('hover', function(e){
-  //   $(e.target).tipsy('show');
-  // });
-  //  d3.json("/poligono" + window.location.pathname.replace(/\/.*?\//, '/'), function(json) {
-  // d3.json("/api/seguranca/RS?o_id=1&ano=2011&content-type=application/json", function(json) {
-  //   region.selectAll("path").data(json.features).enter().append("svg:path").attr("d", path).attr('class', quantize).attr('original-title', function(d) {
-  //     $(this).tipsy({
-  //       gravity: $.fn.tipsy.autoNS
-  //     });
-  //     return d.properties.name + ': ' + d.properties.quant;
-  //   });
-  // });
   $('path').live('click', function(e) {
     console.log($(e.target));
   });
   function fetch(type_id, year) {
-    region.append("svg:text").attr("text-anchor", "middle").attr("x", 220)
-      .attr("y", 150).text('Carregando mapa, aguarde...');
-
     d3.json("/api/seguranca/RS?o_id=$$type&ano=$$year&content-type=application/json".replace('$$year', year).replace('$$type', type_id), function(json) {
       $('#chart svg').remove();
-
       var region = d3.select("#chart").append("svg:svg").attr("id", "region").attr("class", "Reds").attr('width', '450px').attr('height', '400px');
       region.selectAll("path").data(json.features).enter().append("svg:path").attr("d", path).attr('fill', quantize).attr('original-title', function(d) {
         return d.properties.name + ': ' + (d.properties.quant + '').slice(0, 5);
-      }).classed('current', function(d) {
-        return d.properties.name == $('select[name=municipio] option:selected').val();
       }).on("mouseover", function(d, i) {
         d3.select(this).transition().duration(300).style("fill", "#00f1f1");
       }).on("mouseout", function(d, i) {
         d3.select(this).transition().duration(300).style("fill", quantize);
-      });;
+      }).on('click', function(d, i) {
+        window.location = '?' + $.param({
+          ano: $('select[name=ano] option:selected').val(),
+          ocorrencia: $('select[name=ocorrencia] option:selected').val(),
+          municipio: d.properties.name
+        });
+      }).classed('current', function(d) {
+        return d.properties.name == $('select[name=municipio] option:selected').val();
+      });
       $('path').qtip({
         content: {
           attr: 'original-title'
         },
         position: {
-          target: 'mouse'
+          adjust: {
+            mouse: true
+          },
+          target: 'mouse',
+          at: 'top right',
+          my: 'bottom left',
+          adjust: {
+            x: 5,
+            y: -5
+          }
         }
       });
     });
