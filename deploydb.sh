31bcfebb89739ff439b185a655ad8c7ea81ce128
Renato Cron <renato@aware.com.br>
2013-03-03 04:45:58 -0300

    ajuste no deploy, para criar tabela municipio_rate_ano

--- deploydb.sh
+++ deploydb.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 sh script/deploydb.sh ondeacontece
-sh script/populatedb.sh 
-psql -d ondeacontece -f data/analise/municipios.sql 
-psql -d ondeacontece -f data/analise/ocorrencias.sql 
+sh script/populatedb.sh
+psql -d ondeacontece -f data/analise/municipios.sql
+psql -d ondeacontece -f data/analise/ocorrencias.sql
 psql -d ondeacontece -f data/analise/ocorrencias_municipio.sql
 psql -d ondeacontece -f data/analise/municipio_censo.sql
+psql -d ondeacontece -f data/analise/rate_por_municipio_por_ano.sql
