2fa913dec15dc30d4d9f610f8b9b87c120397a33
Renato Cron <renato@aware.com.br>
2013-03-03 04:53:10 -0300

    FIX agora faz o deploy da municipio_ibge_to_ssprs.sql antes da rate_por_municipio_por_ano.sql

--- deploydb.sh
+++ deploydb.sh
@@ -6,4 +6,5 @@ psql -d ondeacontece -f data/analise/municipios.sql
 psql -d ondeacontece -f data/analise/ocorrencias.sql
 psql -d ondeacontece -f data/analise/ocorrencias_municipio.sql
 psql -d ondeacontece -f data/analise/municipio_censo.sql
+psql -d ondeacontece -f data/analise/municipio_ibge_to_ssprs.sql
 psql -d ondeacontece -f data/analise/rate_por_municipio_por_ano.sql
